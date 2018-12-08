f8789020cf29b6cbcc03d792528a29e04bc60197
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-08 20:39:21 +0000

    Update dsd.js

--- dsd.js
+++ dsd.js
@@ -273,4 +273,19 @@ client.on('message', message => {
   }
   });
 
+client.on('message', msg => {
+
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("521062066094604325").join(); 
+    });
+
 client.login(process.env.BOT_TOKEN);
